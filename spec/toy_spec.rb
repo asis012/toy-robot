ad5919d1cf20b018dcade37919b36445f2fa45f5
Ashish Gyawali <asis3005@gmail.com>
2023-10-11 14:34:54 +1100

    add test case for toy to verify initialized value

--- spec/toy_spec.rb
+++ spec/toy_spec.rb
@@ -1,21 +1,28 @@
 require "./lib/toy"
-require 'simplecov'
+require "simplecov"
 SimpleCov.start do
-  add_group 'lib', './lib/toy'
+  add_group "lib", "./lib/toy"
 end
 
 RSpec.describe Toy do
   describe "#exist?" do
     it "initialize toy with all attributes" do
-      toy = Toy.new(2, 2, 'NORTH')
+      toy = Toy.new(2, 2, "NORTH")
       expect(toy.exist?).to eq(true)
     end
-    
+
+    it "check attributes of toy after initialize" do
+      toy = Toy.new(2, 2, "NORTH")
+      expect(toy.x_position).to eq(2)
+      expect(toy.y_position).to eq(2)
+      expect(toy.direction).to eq("NORTH")
+    end
+
     it "initialize toy with x and y position attributes only" do
       toy = Toy.new(2, 2)
       expect(toy.exist?).to eq(true)
     end
-    
+
     it "initialize toy without any attributes" do
       toy = Toy.new
       expect(toy.exist?).to eq(false)
