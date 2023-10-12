240f7810a12124c6ad5cb9eaa75ef910a4d3f1c7
Ashish Gyawali <asis3005@gmail.com>
2023-10-12 14:15:52 +1100

    refactor: use local varibale instead if instance variable

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,7 @@ require_relative "board"
 require_relative "toy"
 require_relative "command"
 require_relative "../config/constant"
+require "pry"
 
 def start_game
   instruction_message = " Enter PLACE X,Y,F to place Toy in x,y position with f direction \n Enter MOVE to move forward \n Enter LEFT to rotate 90 degrees by left \n Enter RIGHT to rotate 90 degrees by right \n Enter REPORT to get final postion of Toy"
@@ -10,39 +11,41 @@ def start_game
   puts "---------------:"
   puts "Enter inital position:"
 
-  @board = Board.new(Constant::BOARD::LENGTH, Constant::BOARD::BREADTH)
-  @toy = Toy.new
+  board = Board.new(Constant::BOARD::LENGTH, Constant::BOARD::BREADTH)
+  toy = Toy.new
+  command = Command.new(board, toy, nil)
 
   loop do
-    command = gets.chomp
-    @command = Command.new(@board, @toy, command)
-    case command
-    when /^PLACE\s*\d,\d,(NORTH|SOUTH|EAST|WEST)*$/
-      error = @command.place
+    user_command = gets.chomp
+    command.command = user_command
+    case user_command
+    when /^PLACE\s+\d,\d,(NORTH|SOUTH|EAST|WEST)*$/
+      error = command.place
       puts "ERROR: #{error}" if error.is_a?(Array) && error.length > 0
 
     when Constant::ACTION::MOVE
-      error = @command.move
+      error = command.move
       puts "ERROR: #{error}" if error.is_a?(Array) && error.length > 0
 
     when Constant::ACTION::LEFT
-      error = @command.left
+      error = command.left
       puts "ERROR: #{error}" if error != ""
 
     when Constant::ACTION::RIGHT
-      error = @command.right
+      error = command.right
       puts "ERROR: #{error}" if error != ""
+
     when Constant::ACTION::REPORT
-      puts @command.report
+      puts command.report
 
     when Constant::ACTION::EXIT
       puts "Exiting the simulation. Thank You!!!"
       break
+
     else
       puts "---------------:"
       puts "Invalid Instruction. Please follow below instruction \n" + instruction_message
       puts "---------------:"
-
     end
   end
 end
